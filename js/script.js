0d9d7b4c9f20420ff0a8647877e7400c82a007a9
Dinidu Sri Nimesha <131584114+dinidusrinimesha@users.noreply.github.com>
2025-05-18 20:50:14 +0530

    Contact form UI updated and Customize with personal data

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function comingSoonFun(sts) {
+    if (sts == 'project'){
+       window.alert("This project is expected to be hosted soon.");
+    } else if(sts == 'contactForm') {
+        window.alert("This feature is under maintanance. Please feel free to contact me via social media.");
+    } else {
+        window.alert("Oops... Something went wrong..!");
+    }
+}
